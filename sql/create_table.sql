5b93c8b5f5dd8332c1de63a3aab614509e0a57a8
Ritmwa Bewarang <bewarangritmwa@gmail.com>
2024-07-15 13:27:24 +0100

    initial commit

---
+++ sql/create_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE university(
+    id SERIAL PRIMARY KEY,
+    country VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    founded INT NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    enrollment BIGINT NOT NULL,
+    link VARCHAR(255) NOT NULL,
+    constraint unique_combination UNIQUE (country, name)
+)
